71cfac26cd823ba1f9949de6d66a737575bf2af5
Jasmin Badža <jasmin.badza@gmail.com>
2019-06-13 04:39:26 +0200

    Staff form restyled.

--- frontend/components/StaffForm.js
+++ frontend/components/StaffForm.js
@@ -3,33 +3,34 @@ import { headers, environment } from "../api.js";
 export default {
   name: "StaffForm",
   template: `
-      <div class="w-full max-w-md mx-auto mt-20">
-        <form @submit.prevent="save" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-            <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
-              Name
-            </label>
-            <input v-model="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Name">
+    <div class="form-container">
+    <h3>Staff</h3>
+      <form @submit.prevent="save">
+        <div class="form-group row">
+          <label for="name" class="col-4 col-form-label">Name</label> 
+          <div class="col-8">
+            <input id="name" name="name" v-model="name" type="text" class="form-control">
           </div>
-          <div class="mb-4">
-            <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
-              Email
-            </label>
-            <input v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Email">
-          </div>
-          <div class="mb-6">
-            <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
-              Phone Number
-            </label>
-            <input v-model="phone_number" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" id="phone_number" type="text" placeholder="Phone Number">
-          </div>
-          <div class="flex">
-            <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit">
-              Save Staff
-            </button>
-          </div>
-        </form>
         </div>
+        <div class="form-group row">
+          <label for="email" class="col-4 col-form-label">Email</label> 
+          <div class="col-8">
+            <input id="email" name="email" v-model="email" type="text" class="form-control">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="phone_number" class="col-4 col-form-label">Phone Number</label> 
+          <div class="col-8">
+            <input id="phone_number" v-model="phone_number" name="phone_number" type="text" class="form-control">
+          </div>
+        </div> 
+        <div class="form-group row">
+          <div class="offset-4 col-8">
+            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
         `,
   data: function() {
     let staffObj = {
